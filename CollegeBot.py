d83a5eedb6ee14854b42079f0e026e8060030bb9
ZXY-CC-3ag13 <ZXY-CC-3ag13@protonmail.com>
2023-04-10 23:18:32 +0545

    Use Github actions

--- CollegeBot.py
+++ CollegeBot.py
@@ -1,29 +1,30 @@
 import datetime
-import time
-import telegram
 import os
+import telegram
 from telegram.ext import Updater, CommandHandler
 
-# set up the bot
+# Get the bot token and chat ID from environment variables
 TOKEN = os.environ.get('BOT_TOKEN')
 CHAT_ID = os.environ.get('CHAT_ID')
+
+# Initialize the bot
 bot = telegram.Bot(token=TOKEN)
 
-# define the labs and their corresponding days
+# Define the labs and their corresponding days
 labs = {
     'Network Programming Lab': [4],
     'Advance Java Programming Lab': [2, 4],
     'Mobile Programming Lab': [1, 3]
 }
 
-# define the message to be sent
-message = "Reminder: {} today at 8am."
+# Define the message to be sent
+message = "Reminder: {} today."
 
-# define a function to check if today is a lab day
-def is_lab_day(day, lab):
-    return day.weekday() in lab
+# Define a function to check if today is a lab day
+def is_lab_day(day, lab_days):
+    return day.weekday() in lab_days
 
-# define a function to send the reminder message
+# Define a function to send the reminder message
 def send_reminder(context):
     now = datetime.datetime.now()
     for lab, days in labs.items():
@@ -31,13 +32,13 @@ def send_reminder(context):
             text = message.format(lab)
             bot.send_message(chat_id=CHAT_ID, text=text)
 
-# set up the job queue
+# Set up the job queue
 updater = Updater(TOKEN, use_context=True)
 job_queue = updater.job_queue
 
-# add the job to send the reminder message at 8am every day
+# Add the job to send the reminder message at 8am every day
 job = job_queue.run_daily(send_reminder, time=datetime.time(hour=8))
 
-# start the bot
+# Start the bot
 updater.start_polling()
 updater.idle()
